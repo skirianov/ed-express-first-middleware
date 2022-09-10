90cf4a816489a344ecea322e6dd130cd044d9ed5
S.Kiri <s.kirianov@gmail.com>
2022-09-10 15:39:54 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const USERS = [
+  {
+    id: 1,
+    name: 'John',
+  },
+  {
+    id: 2,
+    name: 'Jane',
+  },
+  {
+    id: 3,
+    name: 'Joe',
+  },
+
+  {
+    id: 4,
+    name: 'Jack',
+  },
+  {
+    id: 5,
+    name: 'Jill',
+  },
+  {
+    id: 6,
+    name: 'Juan',
+  },
+  {
+    id: 7,
+    name: 'Jenny',
+  },
+  {
+    id: 8,
+    name: 'Jared',
+  },
+  {
+    id: 9,
+    name: 'Jasmine',
+  },
+  {
+    id: 10,
+    name: 'Jared',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
